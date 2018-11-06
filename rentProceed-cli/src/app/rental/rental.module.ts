73f2bd56010e6aec75663b04cd716e213d39631f
PrincewillDavidson <princewill_david@yahoo.com>
2018-11-06 15:20:19 +0000

    Proxy config, rental detail styles

--- rentProceed-cli/src/app/rental/rental.module.ts
+++ rentProceed-cli/src/app/rental/rental.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Routes,RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {NgPipesModule} from 'ngx-pipes';
+
 
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
@@ -8,6 +11,7 @@ import { RentalComponent } from './rental.component';
 
 import {RentalService} from './shared/rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+import {UppercasePipe} from '../common/pipes/uppercase.pipe';
 
 const routes: Routes =  [ {path: 'rentals', component: RentalComponent,
                         children: [{path: '', component: RentalListComponent},
@@ -18,10 +22,12 @@ const routes: Routes =  [ {path: 'rentals', component: RentalComponent,
         RentalListComponent,
         RentalListItemComponent,
         RentalComponent,
-        RentalDetailComponent],
+        RentalDetailComponent,
+        UppercasePipe],
         
     imports: [CommonModule,
-    RouterModule.forChild(routes)],
+    RouterModule.forChild(routes), HttpClientModule,
+    NgPipesModule],
     providers:[RentalService]
 })
 export class RentalModule {}
